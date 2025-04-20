b6e6a1e88020c8c825034aeeca8ac0b2dbabc09f
rahat-495 <md8702687@gmail.com>
2025-04-20 21:56:36 +0600

    Rahat : user register are fully done !

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,8 +1,10 @@
 
+import config from "../../config";
 import AppError from "../../errors/AppErrors";
 import { sendImageToCloudinary } from "../../utils/sendImageToCloudinary";
 import { usersModel } from "../user/user.modules";
 import { TRegisterUser } from "./auth.interfaces";
+import jwt from "jsonwebtoken" ;
 
 const createUserIntoDb = async (file : any , payload : TRegisterUser) => {
     const isUserAlreadyExist = await usersModel.findOne({email : payload?.email , phoneNumber : payload?.phoneNumber}) ;
@@ -16,9 +18,17 @@ const createUserIntoDb = async (file : any , payload : TRegisterUser) => {
         const {secure_url} = await sendImageToCloudinary(imageName , path) as any ;
         payload.profileImage = secure_url ;
     }
-
+    
     const result = await usersModel.create(payload) ;
-    return result ;
+    
+    if(!result){
+        throw new AppError(400 , "User creation failed !") ;
+    }
+
+    const jwtPayload = { email : result?.email , role : result?.role , userId : result?._id , phoneNumber : result?.phoneNumber } ;
+    const accessToken = await jwt.sign(jwtPayload , config.jwtAccessSecret as string , {expiresIn : "1d"}) ;
+    const refreshToken = await jwt.sign(jwtPayload , config.jwtRefreshSecret as string , {expiresIn : "365d"}) ;
+    return {accessToken , refreshToken} ;
 }
 
 export const authServices = {
