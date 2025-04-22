51158f62b33b36b5baf9e261e49a827e22b10fbc
rahat-495 <md8702687@gmail.com>
2025-04-22 21:46:30 +0600

    Rahat : dietary preference creating are done !

--- src/app/modules/preferences/preferences.services.ts
+++ src/app/modules/preferences/preferences.services.ts
@@ -1,9 +1,18 @@
 
+import AppError from "../../errors/AppErrors";
 import { TDietaryPreference } from "./preferences.interfaces"
+import { dietaryPreferencesModel } from "./preferences.model";
+import http from "http-status-codes"
 
 const createDietaryPreferenceIntoDb = async (userId : string , payload : TDietaryPreference) => {
-    console.log(userId , payload);
-    return null ;
+    const isUserAlreadyHaveDietaryPreference = await dietaryPreferencesModel.findOne({userId}) ;
+    
+    if(isUserAlreadyHaveDietaryPreference){
+        throw new AppError(http.BAD_REQUEST , "User already have dietary preference") ;
+    }
+
+    const result = await dietaryPreferencesModel.create({...payload , userId}) ;
+    return result ;
 }
 
 export const dietaryPreferenceServices = {
--- src/app/modules/preferences/preferences.validations.ts
+++ src/app/modules/preferences/preferences.validations.ts
@@ -19,7 +19,6 @@ const createDietaryPreferenceValidationSchema = z.object({
             "SugarFree",
             "Organic"
           ]),
-        userId: z.string({ message: "userId is required" }),
     })
 })
 
