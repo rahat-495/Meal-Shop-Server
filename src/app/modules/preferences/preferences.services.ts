bea45ce1d6ccf5bd22530e62392ac9f708838a6e
rahat-495 <md8702687@gmail.com>
2025-04-22 21:52:51 +0600

    Rahat : updating dietary preference service are done !

--- src/app/modules/preferences/preferences.services.ts
+++ src/app/modules/preferences/preferences.services.ts
@@ -8,13 +8,25 @@ const createDietaryPreferenceIntoDb = async (userId : string , payload : TDietar
     const isUserAlreadyHaveDietaryPreference = await dietaryPreferencesModel.findOne({userId}) ;
     
     if(isUserAlreadyHaveDietaryPreference){
-        throw new AppError(http.BAD_REQUEST , "User already have dietary preference") ;
+        throw new AppError(http.BAD_REQUEST , "User already have dietary preference !") ;
     }
 
     const result = await dietaryPreferencesModel.create({...payload , userId}) ;
     return result ;
 }
 
+const updateDietaryPreferenceIntoDb = async (userId : string , payload : TDietaryPreference) => {
+    const isUserAlreadyHaveDietaryPreference = await dietaryPreferencesModel.findOne({userId}) ;
+
+    if(!isUserAlreadyHaveDietaryPreference){
+        throw new AppError(http.NOT_FOUND , "User have not any dietary preference !") ;
+    }
+
+    const result = await dietaryPreferencesModel.findOneAndUpdate({userId} , {...payload} , {new : true}) ;
+    return result ;
+}
+
 export const dietaryPreferenceServices = {
-    createDietaryPreferenceIntoDb
+    createDietaryPreferenceIntoDb ,
+    updateDietaryPreferenceIntoDb ,
 }
