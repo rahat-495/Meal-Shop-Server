11e2beb2ba729d3e4c59012976684371a226a0a5
rahat-495 <md8702687@gmail.com>
2025-04-24 22:05:42 +0600

    Rahat : user meal preference crud oparations are done !

--- src/app/modules/mealPreferences/mealPreferences.controllers.ts
+++ src/app/modules/mealPreferences/mealPreferences.controllers.ts
@@ -4,13 +4,45 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { mealPreferenceServices } from "./mealPreferences.services";
 
+const getAllMealPreferences : RequestHandler = catchAsync(async (req , res) => {
+    const result = await mealPreferenceServices.getAllMealPreferencesFromDb(req.query) ;
+    if(result){
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Meal preferences are retrived success full !"}) ;
+    }
+})
+
+const getSingleMealPreference : RequestHandler = catchAsync(async (req , res) => {
+    const result = await mealPreferenceServices.getSingleMealPreferenceFromDb(req.params.id) ;
+    if(result){
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Meal preference are retrived success full !"}) ;
+    }
+})
+
 const createMealPreference : RequestHandler = catchAsync(async (req , res) => {
     const result = await mealPreferenceServices.createMealPreferenceIntoDb(req.user.userId , req.body) ;
     if(result){
-        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Dietary preference created success full !"}) ;
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Meal preference created success full !"}) ;
+    }
+})
+
+const updateMealPreference : RequestHandler = catchAsync(async (req , res) => {
+    const result = await mealPreferenceServices.updateMealPreferenceIntoDb(req.params.id , req.body) ;
+    if(result){
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Meal preference updated success full !"}) ;
+    }
+})
+
+const deleteMealPreference : RequestHandler = catchAsync(async (req , res) => {
+    const result = await mealPreferenceServices.deleteMealPreferenceFromDb(req.params.id) ;
+    if(result){
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Meal preference deleted success full !"}) ;
     }
 })
 
 export const mealPreferenceControllers = {
     createMealPreference ,
+    updateMealPreference ,
+    deleteMealPreference ,
+    getAllMealPreferences ,
+    getSingleMealPreference ,
 }
